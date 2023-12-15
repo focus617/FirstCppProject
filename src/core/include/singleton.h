e70d014b681ef11ff52877adc436cdbf8cf88c70
Xu Zhiyong <13980668829@163.com>
2023-12-15 12:50:44 +0800

    Refactor Singleton with thread safe.

--- src/core/include/singleton.h
+++ src/core/include/singleton.h
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <mutex>
 
 class Singleton {
 private:
     static Singleton* one_and_only_instance;
     static int unique_id;
+    static std::mutex m_LogMutex;
+
     Singleton(const int num){
         unique_id = num;
     }
 
 public:
     static Singleton* get_instance(const int num) {
+        // thread safe
+        std::scoped_lock lock(m_LogMutex);
+
         if(one_and_only_instance == nullptr) {
             std::cout << "creating a new instance" << std::endl;
             // lazy initialization
@@ -23,10 +29,11 @@ public:
     void operator=(const Singleton &) = delete;
 
     void PrintUniqueID() {
-        std::cout << "Current Instance's unique id: " << this->unique_id << std::endl;
+        std::cout << "Instance's unique id: " << this->unique_id << std::endl;
     }
 
 };
 
 Singleton* Singleton::one_and_only_instance = nullptr;
 int Singleton::unique_id = 0;
+std::mutex Singleton::m_LogMutex;
