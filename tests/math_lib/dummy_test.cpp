c75428b2505421f28aece9a4b0fd5e0206ebeb65
Xu Zhiyong <13980668829@163.com>
2023-12-18 22:31:04 +0800

    Add test case with preset parameters in math_test.

--- tests/math_lib/dummy_test.cpp
+++ tests/math_lib/dummy_test.cpp
@@ -16,42 +16,42 @@ public:
   {
     m_tested_dummy = std::make_unique<Dummy>(m_mock_foo);
   }
-  ~Dummy_Test_Fixture(){}
+  ~Dummy_Test_Fixture() {}
 
   void SetUp();
   void TearDown();
 };
 
 void Dummy_Test_Fixture::SetUp()
-{    
+{
 }
 
 void Dummy_Test_Fixture::TearDown()
-{    
+{
 }
 
 // 测试函数的返回值
 TEST_F(Dummy_Test_Fixture, dummy_int)
 {
-    std::string str {"Hello World."};
+  std::string str{"Hello World."};
 
-    EXPECT_CALL(m_mock_foo, foo_int(str)).Times(1).WillOnce(testing::Return(str.size()));
-    int result = m_tested_dummy->dummy_int(str);
+  EXPECT_CALL(m_mock_foo, foo_int(str)).Times(1).WillOnce(testing::Return(str.size()));
+  int result = m_tested_dummy->dummy_int(str);
 
-    ASSERT_EQ(result, str.size());
+  ASSERT_EQ(result, str.size());
 }
 
 // 测试函数的引用参数被正确修改
 TEST_F(Dummy_Test_Fixture, dummy_str)
 {
-    std::string input_str {"Hello World."};
-    std::string output_str {"ChangedString"};
+  std::string input_str{"Hello World."};
+  std::string output_str{"ChangedString"};
 
-    // testing::_表示忽略函数的参数，不需要检查
-    EXPECT_CALL(m_mock_foo, foo_str(testing::_)).WillOnce(testing::SetArgReferee<0>(output_str));
-    m_tested_dummy->dummy_str(input_str);
+  // testing::_表示忽略函数的参数，不需要检查
+  EXPECT_CALL(m_mock_foo, foo_str(testing::_)).WillOnce(testing::SetArgReferee<0>(output_str));
+  m_tested_dummy->dummy_str(input_str);
 
-    ASSERT_EQ(input_str, output_str);
+  ASSERT_EQ(input_str, output_str);
 }
 
 ACTION(throwError)
@@ -69,23 +69,23 @@ void invoke_function()
 // 测试函数调用将丢出异常
 TEST_F(Dummy_Test_Fixture, dummy_throw)
 {
-    // mock throw exception
-    EXPECT_CALL(m_mock_foo, foo_throw()).WillOnce(throwError());
+  // mock throw exception
+  EXPECT_CALL(m_mock_foo, foo_throw()).WillOnce(throwError());
 
-    // mock invoke function
-    // EXPECT_CALL(m_mock_foo, foo_throw()).WillOnce(testing::Invoke(invoke_function));
+  // mock invoke function
+  // EXPECT_CALL(m_mock_foo, foo_throw()).WillOnce(testing::Invoke(invoke_function));
 
-    ASSERT_THROW(m_tested_dummy->dummy_throw(), std::runtime_error);
+  ASSERT_THROW(m_tested_dummy->dummy_throw(), std::runtime_error);
 }
 
 // 测试函数调用回调函数
 TEST_F(Dummy_Test_Fixture, dummy_callback)
 {
-    // mock callback function
-    testing::MockFunction<void(void)> testFunc;
-    
-    EXPECT_CALL(m_mock_foo, foo_callback_method(testing::_)).Times(1).WillOnce(testing::InvokeArgument<0>());
-    EXPECT_CALL(testFunc, Call());
+  // mock callback function
+  testing::MockFunction<void(void)> testFunc;
 
-    m_tested_dummy->dummy_callback(testFunc.AsStdFunction());
+  EXPECT_CALL(m_mock_foo, foo_callback_method(testing::_)).Times(1).WillOnce(testing::InvokeArgument<0>());
+  EXPECT_CALL(testFunc, Call());
+
+  m_tested_dummy->dummy_callback(testFunc.AsStdFunction());
 }
--- tests/math_lib/math_test.cpp
+++ tests/math_lib/math_test.cpp
@@ -1,13 +1,36 @@
 #include <gtest/gtest.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <tuple>
+
 #include "xuzy_math.h"
 
-TEST(MathTest, BasicAddtions) {
-  int result = xuzy::add(7, 12);
+class Math_Test_With_Param: public testing::TestWithParam<std::tuple<int, int, int>>
+{
+  // You can implement all the usual fixture class member here.
+  // To access the test parameter, call GetParam() from class TestWithParam<T>.
+};
+
+TEST_P(Math_Test_With_Param, add_test_equal) {
+  // access the test parameter
+  auto [a, b, c] = GetParam();
+
   // Expect equality.
-  EXPECT_EQ(result, 19);
+  EXPECT_EQ(xuzy::add(a, b), c) << "Oh no, a mistake!";
 }
 
-TEST(MathTest, AssertNoEqual) {
-  // Expect NOT equality.
-  ASSERT_NE(xuzy::add(7, 12), 18) << "Oh no, a mistake!";
-}
+INSTANTIATE_TEST_SUITE_P(PositiveNumbers,
+                          Math_Test_With_Param,
+                          testing::Values(
+                            std::tuple{1, 2, 3},
+                            std::tuple{2, 5, 7}
+                          )
+                        );
+
+INSTANTIATE_TEST_SUITE_P(NegativeNumbers,
+                          Math_Test_With_Param,
+                          testing::Values(
+                            std::tuple{-1, -2, -3},
+                            std::tuple{-2, 5, 3}
+                          )
+                        );
