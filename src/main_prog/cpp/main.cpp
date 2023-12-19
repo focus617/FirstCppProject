c10b2552687f7f5495ad58e63debd74b9e5c1e3b
Xu Zhiyong <13980668829@163.com>
2023-12-19 19:19:22 +0800

    Improve command line parsing for more flags.

--- src/main_prog/cpp/main.cpp
+++ src/main_prog/cpp/main.cpp
@@ -11,9 +11,16 @@
 
 using xuzy::Log;
 
+void test_logger();
+void test_singleton();
+
+// 从配置文件读取配置参数
+void load_configure(const std::string& filename);
+
 // Define the flag 标志
 // 参数: 1.标志类型 2.标志名 3.默认值 4.标志描述
 ABSL_FLAG(bool, verbose, false, "Enable verbose mode");
+ABSL_FLAG(std::optional<std::string>, ConfigFile, std::nullopt, "Settings from a file.");
 
 int main(int argc, char *argv[])
 {
@@ -29,30 +36,58 @@ int main(int argc, char *argv[])
     // 读取bool串类型标志
     LOG_INFO("flag verbose: %s", absl::GetFlag(FLAGS_verbose) ? "true" : "false");
 
+    if (absl::GetFlag(FLAGS_ConfigFile).has_value())
+    {
+        // flag was set on command line
+        std::string config_file_name = absl::GetFlag(FLAGS_ConfigFile).value();
+        LOG_INFO("ConfigFile flags: %s", config_file_name.c_str());
+
+        load_configure(config_file_name);
+    }
+    else
+    {
+        // flag was not passed on command line
+        // use default configuration
+    }
+
     /*
-        Log::SetLevel(Log::LevelTrace);
-
-        int i{0};
-        LOG_TRACE("%d: Test Log Level %s", i++, "Trace");
-        LOG_DEBUG("%d: Test Log Level %s", i++, "Debug");
-        LOG_INFO("%d: Test Log Level %s", i++, "Info");
-        LOG_WARN("%d: Test Log Level %s", i++, "Warn");
-        LOG_ERROR("%d: Test Log Level %s", i++, "Error");
-        LOG_CRITICAL("%d: Test Log Level %s", i++, "Critical");
-
-        int a = 12;
-        int b = 56;
-        int result = xuzy::add(a, b);
-        LOG_INFO("%d + %d = %d", a, b, result);
-
-        Singleton *s1 = Singleton::get_instance(10);
-        s1->print_unique_id();
-        std::cout << "S1 memory location: " << s1 << std::endl;
-
-        Singleton *s2 = Singleton::get_instance(5);
-        s2->print_unique_id();
-        std::cout << "S2 memory location: " << s2 << std::endl;
+        test_logger();
+        test_singleton();
     */
 
     return EXIT_SUCCESS;
+}
+
+void load_configure(const std::string& filename)
+{
+    LOG_INFO("Load configure from file: %s", filename.c_str());
+}
+
+void test_logger()
+{
+    Log::SetLevel(Log::LevelTrace);
+
+    int i{0};
+    LOG_TRACE("%d: Test Log Level %s", i++, "Trace");
+    LOG_DEBUG("%d: Test Log Level %s", i++, "Debug");
+    LOG_INFO("%d: Test Log Level %s", i++, "Info");
+    LOG_WARN("%d: Test Log Level %s", i++, "Warn");
+    LOG_ERROR("%d: Test Log Level %s", i++, "Error");
+    LOG_CRITICAL("%d: Test Log Level %s", i++, "Critical");
+
+    int a = 12;
+    int b = 56;
+    int result = xuzy::add(a, b);
+    LOG_INFO("%d + %d = %d", a, b, result);
+}
+
+void test_singleton()
+{
+    Singleton *s1 = Singleton::get_instance(10);
+    s1->print_unique_id();
+    std::cout << "S1 memory location: " << s1 << std::endl;
+
+    Singleton *s2 = Singleton::get_instance(5);
+    s2->print_unique_id();
+    std::cout << "S2 memory location: " << s2 << std::endl;
 }
